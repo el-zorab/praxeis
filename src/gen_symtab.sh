b8c4ed15e1612881069e6470b6811147300674f0
Vlad-Robert Baciu <robertvladbaciu@gmail.com>
2022-09-06 23:52:29 +0300

    Hacky workaround to get correct symtab on current build

--- src/gen_symtab.sh
+++ src/gen_symtab.sh
@@ -4,7 +4,7 @@ printf "#include <stddef.h>\n#include \"symtab/symtab.h\"\n\n" > $2
 printf "typedef struct {\n    uint64_t address;\n    const char* name;\n} symbol_t;\n\n" >> $2
 printf "__attribute__((section(\".symbol_table\")))\n" >> $2
 printf "static const symbol_t symtab[] = {\n" >> $2
-nm $1 | grep " T " | awk '{ print "    { .address = 0x"$1", .name = \""$3"\" }," }' | sort >> $2
+nm $1 | grep " T " | grep -v "text_start_address" | awk '{ print "    { .address = 0x"$1", .name = \""$3"\" }," }' | sort >> $2
 printf "    { .address = UINT64_MAX, .name = \"\" }\n};\n\n" >> $2
 printf "const char* get_symtab_function_name(uint64_t address) {\n" >> $2
 printf "    for (uint64_t i = 0; symtab[i].address < UINT64_MAX; i++) {\n" >> $2
