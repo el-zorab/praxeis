1e4f922ca942ad233c3d73d4b2b0c60e358fcc01
Vlad-Robert Baciu <robertvladbaciu@gmail.com>
2022-09-08 10:30:51 +0300

    Put limine requests in .limine_reqs ELF section

--- src/kernel.c
+++ src/kernel.c
@@ -16,53 +16,34 @@
 #include "x86/interrupts/idt.h"
 #include "x86/smp/smp.h"
 
-static volatile struct limine_5_level_paging_request five_level_paging_request = {
-    .id = LIMINE_5_LEVEL_PAGING_REQUEST,
-    .revision = 0
-};
-
-static volatile struct limine_bootloader_info_request bootloader_info_request = {
-    .id = LIMINE_BOOTLOADER_INFO_REQUEST,
-    .revision = 0
-};
-
-static volatile struct limine_framebuffer_request framebuffer_request = {
-    .id = LIMINE_FRAMEBUFFER_REQUEST,
-    .revision = 0
-};
-
-static volatile struct limine_hhdm_request hhdm_request = {
-    .id = LIMINE_HHDM_REQUEST,
-    .revision = 0
-};
-
-static volatile struct limine_kernel_address_request kernel_address_request = {
-    .id = LIMINE_KERNEL_ADDRESS_REQUEST,
-    .revision = 0
-};
-
-static volatile struct limine_memmap_request memmap_request = {
-    .id = LIMINE_MEMMAP_REQUEST,
-    .revision = 0
-};
-
-static volatile struct limine_rsdp_request rsdp_request = {
-    .id = LIMINE_RSDP_REQUEST,
-    .revision = 0
-};
-
-static volatile struct limine_smp_request smp_request = {
-    .id = LIMINE_SMP_REQUEST,
-    .revision = 0
+__attribute__((section(".limine_reqs"))) 
+uintptr_t requests[] = {
+    (uintptr_t) &((struct limine_5_level_paging_request)  { .id = LIMINE_5_LEVEL_PAGING_REQUEST,  .revision = 0 }),
+    (uintptr_t) &((struct limine_bootloader_info_request) { .id = LIMINE_BOOTLOADER_INFO_REQUEST, .revision = 0 }),
+    (uintptr_t) &((struct limine_framebuffer_request)     { .id = LIMINE_FRAMEBUFFER_REQUEST,     .revision = 0 }),
+    (uintptr_t) &((struct limine_hhdm_request)            { .id = LIMINE_HHDM_REQUEST,            .revision = 0 }),
+    (uintptr_t) &((struct limine_kernel_address_request)  { .id = LIMINE_KERNEL_ADDRESS_REQUEST,  .revision = 0 }),
+    (uintptr_t) &((struct limine_memmap_request)          { .id = LIMINE_MEMMAP_REQUEST,          .revision = 0 }),
+    (uintptr_t) &((struct limine_rsdp_request)            { .id = LIMINE_RSDP_REQUEST,            .revision = 0 }),
+    (uintptr_t) &((struct limine_smp_request)             { .id = LIMINE_SMP_REQUEST,             .revision = 0 }),
+    (uintptr_t) NULL
 };
 
 uint64_t hhdm_offset;
 
 void kmain(void) {
-    struct limine_bootloader_info_response *bootloader_info = bootloader_info_request.response;
-    struct limine_framebuffer_response *framebuffer_response = framebuffer_request.response;
+    uint8_t requests_counter = 0;
+    // These must be in the same order as in the requests array
+    struct limine_5_level_paging_response  *five_level_paging_response = ((struct limine_5_level_paging_request*)  requests[requests_counter++])->response;
+    struct limine_bootloader_info_response *bootloader_info_response   = ((struct limine_bootloader_info_request*) requests[requests_counter++])->response;
+    struct limine_framebuffer_response     *framebuffer_response       = ((struct limine_framebuffer_request*)     requests[requests_counter++])->response;
+    struct limine_hhdm_response            *hhdm_response              = ((struct limine_hhdm_request*)            requests[requests_counter++])->response;
+    struct limine_kernel_address_response  *kernel_address_response    = ((struct limine_kernel_address_request*)  requests[requests_counter++])->response;
+    struct limine_memmap_response          *memmap_response            = ((struct limine_memmap_request*)          requests[requests_counter++])->response;
+    struct limine_rsdp_response            *rsdp_response              = ((struct limine_rsdp_request*)            requests[requests_counter++])->response;
+    struct limine_smp_response             *smp_response               = ((struct limine_smp_request*)             requests[requests_counter++])->response;
 
-    hhdm_offset = hhdm_request.response->offset;
+    hhdm_offset = hhdm_response->offset;
 
     if (framebuffer_response == NULL || framebuffer_response->framebuffer_count < 1) panic("No framebuffer available", false);
     if (framebuffer_response->framebuffers[0]->bpp != 32) panic("Framebuffer bpp does not equal 32", false);
@@ -70,7 +51,7 @@ void kmain(void) {
     fb_init(framebuffer_response->framebuffers[0], 32, 32, 32, 32);
     fb_draw_image(fb_get_width() - IMAGE_WIDTH, 0);
     
-    printf("\nPraxeis booted by %s v%s\n\n", bootloader_info->name, bootloader_info->version);
+    printf("\nPraxeis booted by %s v%s\n\n", bootloader_info_response->name, bootloader_info_response->version);
 
     uint32_t eax, ebx, ecx, edx;
     cpuid(0, 0, &eax, &ebx, &ecx, &edx);
@@ -79,12 +60,12 @@ void kmain(void) {
 
     gdt_init();
     idt_init();
-    pmm_init(memmap_request.response);
-    vmm_init(memmap_request.response, kernel_address_request.response, five_level_paging_request.response);
-    acpi_init(rsdp_request.response);
+    pmm_init(memmap_response);
+    vmm_init(memmap_response, kernel_address_response, five_level_paging_response);
+    acpi_init(rsdp_response);
     madt_init();
     hpet_init();
-    smp_init(smp_request.response);
+    smp_init(smp_response);
 
     panic("Reached end of kernel entry point", true);
 }
